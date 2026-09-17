8341951180db8f2c300d5292c4a4b1a363c3b2d1
Iason SK <jason.skk98@gmail.com>
2026-09-17 19:12:46 +0200

    cleanup

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
-// use std::sync::atomic::{AtomicI8, Ordering};
+/// This software implements the romantic relationship simulator coding challenge
+/// This is a linux multithreadded application.
+/// The relationship state machine statuses are expresed as log events in the CLI.
+/// Iason Svoronos - Kanavas 20260917
+
 use std::sync::{Arc, Mutex};
 use std::time::{Duration};
 use chrono::Local;
@@ -33,22 +37,19 @@ pub fn init() -> Result<(), SetLoggerError> {
 
 #[derive(Debug,Clone)]
 struct Relationship {
-    health: Arc<Mutex<i8>>,       // 0-100, clamp on write
-    // last_recovery: Arc<Mutex<Instant>>,
+    health: Arc<Mutex<i8>>,  // 0-100, clamp on write
 }
 
 impl Relationship {
     fn new() -> Relationship {
         Relationship {
             health: Arc::new(Mutex::new(100)),
-            // last_recovery: Arc<Mutex<Instant>>,
-            // log: Logger,
         }
     }
     fn apply_delta(&self, delta:i8, actor:&str, description: &str) {
         let mut health = self.health.lock().unwrap();
         *health = (*health + delta).clamp(0, 100);
-        // force the result into the [0, 100] range. If health+delta is -5, this returns 0. If it's 130, returns 100. Prevents health going out of bounds.
+        // force the result into the [0, 100] range. If health+delta is -5, this returns 0. If it is 130, returns 100. Prevents health going out of bounds.
         // Writes new back into the atomic. Again SeqCst for consistent ordering across threads.
         log::info!("[{actor}] {description} ({delta:+}) -> health = {}", *health);
     }
@@ -56,54 +57,41 @@ impl Relationship {
 
 trait Actor {
     fn act(&self, rel: &Relationship);  // called in a loop on its own thread
-    // fn led(&self) -> LedChannel;        // which LED reports this actor
 }
 
 impl Actor for Boyfriend {
     fn act(&self, rel: &Relationship) {
-        let (delta, description) =  match rand::random_range(0..3) { // max range = Boyfriend number of impl fn, as range is exclusive
+        let (delta, description) =  match rand::random_range(0..3) {  // max range = Boyfriend number of impl fn, as range is exclusive
             0 => Boyfriend::used_word_optimised_too_often(),
             1 => Boyfriend::explained_something_nobody_asked_about(),
             2 => Boyfriend::actually_did_something_nice(),
-            _ => unreachable!("Actor for Boyfriend: Incorrect range of state list. Must be 0..3."), //range is 0..3, compiler can't prove exhaustiveness itself
+            _ => unreachable!("Actor for Boyfriend: Incorrect range of state list. Must be 0..3."),  // range is 0..3, compiler can not prove exhaustiveness itself
         };
         rel.apply_delta(delta, "Boyfriend", description);
     }
-
-    // fn led(&self) -> LedChannel {
-    //     LedChannel::Blue
-    // }
 }
 
 impl Actor for Girlfriend {
     fn act(&self, rel: &Relationship) {
-        let (delta, description) =  match rand::random_range(0..3) { // max range = Girlfriend number of impl fn, as range is exclusive
+        let (delta, description) =  match rand::random_range(0..3) {  // max range = Girlfriend number of impl fn, as range is exclusive
             0 => Girlfriend::planned_a_nice_evening(),
             1 => Girlfriend::gave_the_silent_treatment(),
             2 => Girlfriend::forgave_something(),
-            _ => unreachable!("Actor for Girlfriend: Incorrect range of state list. Must be 0..3."), //range is 0..3, compiler can't prove exhaustiveness itself
+            _ => unreachable!("Actor for Girlfriend: Incorrect range of state list. Must be 0..3."),  // range is 0..3, compiler can not prove exhaustiveness itself
         };
         rel.apply_delta(delta, "Girlfriend", description);
     }
-    
-    // fn led(&self) -> LedChannel {
-    //     LedChannel::Blue
-    // }
 }
 
 impl Actor for OtherHotGuy {
     fn act(&self, rel: &Relationship) {
-        let (delta, description) =  match rand::random_range(0..2) { // max range = OtherHotGuy number of impl fn, as range is exclusive
+        let (delta, description) =  match rand::random_range(0..2) {  // max range = OtherHotGuy number of impl fn, as range is exclusive
             0 => OtherHotGuy::liked_a_story(),
             1 => OtherHotGuy::showed_up_at_the_gym(),
-            _ => unreachable!("Actor for OthoerHotGuy: Incorrect range of state list. Must be 0..2."), //range is 0..2, compiler can't prove exhaustiveness itself
+            _ => unreachable!("Actor for OthoerHotGuy: Incorrect range of state list. Must be 0..2."),  // range is 0..2, compiler can not prove exhaustiveness itself
         };
         rel.apply_delta(delta, "OtherHotGuy", description);
     }
-    
-    // fn led(&self) -> LedChannel {
-    //     LedChannel::Blue
-    // }
 }
 
 // -> Blue LED
@@ -156,11 +144,10 @@ impl OtherHotGuy {
 
 fn main() -> ExitCode {
 
-    init().unwrap(); // init logger
+    init().unwrap();  // init logger
     let rel = Relationship::new();
     
     while *rel.health.lock().unwrap() != 0 {
-        // let random_gen: u64  = rand::random_range(200..2000); // does not update for now
         let bf_random_gen: u64  = rand::random_range(200..2000);
         let gf_random_gen: u64  = rand::random_range(200..2000);
         let ohg_random_gen: u64  = rand::random_range(200..2000); 
